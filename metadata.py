4675573a226cbbcec298a0024a015f713d09cf59
Philippe Trempe <philippe-2.trempe@polymtl.ca>
2017-07-02 16:17:36 -0400

    Added project metadata.

---
+++ metadata.py
@@ -0,0 +1,12 @@
+
+class Metadata(object):
+    name = "netception",
+    version = "0.0.0",
+    description = "A neural network inception library",
+    author = "Philippe Trempe",
+    author_email = "ph.trempe@gmail.com",
+    license = "MIT",
+    repository_url = "https://github.com/PhTrempe/{}".format(name),
+    download_url = "https://github.com/PhTrempe/{}/archive/{}.tar.gz".format(
+        name, version),
+    keywords = ["neural", "network", "inception", "machine", "learning"]
